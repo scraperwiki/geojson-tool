86b2a346bf6c69c52d947c08939b4520a1b32313
David Jones <drj@pobox.com>
2014-08-18 14:57:25 +0000

    Remove some of the non-compliant underscores

--- http/input_box.js
+++ http/input_box.js
@@ -1,13 +1,13 @@
 // :todo(drj): move to scraperwiki.js
-var smartAlert = function(message, technical_details) {
-  var report_btn = '<span id="report_btn">Click the big red <strong>Help</strong> button for assistance</span>'
-  var detail_btn = '<a id="detail_btn">click here to show technical details</a>'
-  scraperwiki.alert(message, '<p class="actions">' + report_btn + ' or ' +
-    detail_btn + '</p><pre>' + technical_details + '\n\n' +
-    obtain_stack() + '</pre>', true)
+var smartAlert = function(message, technicalDetails) {
+  var reportButton = '<span id="report_btn">Click the big red <strong>Help</strong> button for assistance</span>'
+  var detailButton = '<a id="detail_btn">click here to show technical details</a>'
+  scraperwiki.alert(message, '<p class="actions">' + reportButton + ' or ' +
+    detailButton + '</p><pre>' + technicalDetails + '\n\n' +
+    obtainStack() + '</pre>', true)
 }
 
-var obtain_stack = function() {
+var obtainStack = function() {
   return "Origin:\n\n" + printStackTrace().slice(4).join("\n");
 }
 
