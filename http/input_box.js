bf5876f44a93479c81276bcb445693cdf9b5240c
David Jones <drj@pobox.com>
2014-08-18 14:49:44 +0000

    Remove dead code

--- http/input_box.js
+++ http/input_box.js
@@ -1,12 +1,9 @@
 // :todo(drj): move to scraperwiki.js
 var smart_alert = function(message, technical_details){
-  // var report_btn = '<a class="btn btn-mini" id="report_btn">Report this as a bug</a>'
   var report_btn = '<span id="report_btn">Click the big red <strong>Help</strong> button for assistance</span>'
-  // var detail_btn = '<a class="btn btn-mini" id="detail_btn">Show technical details</a>'
   var detail_btn = '<a id="detail_btn">click here to show technical details</a>'
   scraperwiki.alert(message, '<p class="actions">' + report_btn + ' or ' +
     detail_btn + '</p><pre>' + technical_details + '\n\n' + obtain_stack() + '</pre>', true)
-    //send_status_to_intercom(technical_details)
 }
 
 var obtain_stack = function() {
